48f4299294a464e37992d4412d207d9aa366a8f6
Ewelina <94842711+EwelinaBul928928@users.noreply.github.com>
2025-12-05 15:02:55 +0100

    Update start_bot.bat

--- start_bot.bat
+++ start_bot.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal
 
-REM === Ścieżka do JDK i bibliotek Selenium ===
+REM 
 set SRC=.
 set LIB=lib
 set CP=%SRC%;%LIB%\*
 
-REM === Kompiluj main.java ===
+REM 
 echo [INFO] Kompiluję plik main.java...
 javac -cp "%CP%" main.java
 
@@ -16,7 +16,7 @@ IF %ERRORLEVEL% NEQ 0 (
     exit /b 1
 )
 
-echo [INFO] Uruchamiam bota...
+echo [INFO] Uruchamiam bota
 java -cp "%CP%" main
 
 echo [INFO] Bot zakończył działanie.
